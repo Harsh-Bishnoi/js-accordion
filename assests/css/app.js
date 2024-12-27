587a800af55c6265050e059d44113cc6a639d681
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2024-12-27 10:24:22 +0530

    add arrow

--- assests/css/app.js
+++ assests/css/app.js
@@ -1,15 +1,16 @@
-const accordionTittles = document.querySelectorAll(".accordionTittle");
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+accordionHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+        const isActive = header.classList.contains('active');
 
-accordionTittles.forEach((accordionTittle) => {
-    accordionTittle.addEventListener("click", () => {
-        if (accordionTittle.classList.contains("is-open")) {
-            accordionTittle.classList.remove("is-open");
-        } else {
-            const accordionTittlesWithIsOpen = document.querySelectorAll(".is-open");
-            accordionTittlesWithIsOpen.forEach((accordionTittleWithIsOpen) => {
-                accordionTittleWithIsOpen.classList.remove("is-open");
-            })
-            accordionTittle.classList.add("is-open");
+        document.querySelectorAll('.accordion-header.active').forEach(openHeader => {
+            openHeader.classList.remove('active');
+            openHeader.nextElementSibling.style.display = 'none';
+        });
+
+        if (!isActive) {
+            header.classList.add('active');
+            header.nextElementSibling.style.display = 'block';
         }
-    })
-})
+    });
+});
